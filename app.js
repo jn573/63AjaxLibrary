8f682a45f4260862cd4a46bd8dc3f0974f6ed4c1
James <cheekywena@gmail.com>
2021-12-05 20:28:43 +0200

    Added post, delete, update

--- app.js
+++ app.js
@@ -6,6 +6,13 @@ const data = {
     "body": "Some random text"
 }
 
+const patch = {
+    "userId": 101,
+    "id": 1,
+    "title": "A custom posted message",
+    "body": "Some random text"
+}
+
 // http.get('https://jsonplaceholder.typicode.com/posts/1',function(err,posts) {
 //     if (err) {
 //     console.log(err)
@@ -14,12 +21,28 @@ const data = {
 //     }
 // })
 
-http.post('https://jsonplaceholder.typicode.com/posts', data, function (err, posts) {
+// http.post('https://jsonplaceholder.typicode.com/posts', data, function (err, posts) {
+//     if (err) {
+//         console.log(err)
+//     } else {
+//         console.log(posts)
+//     }
+// })
+//
+// http.update('https://jsonplaceholder.typicode.com/posts/1', patch, function (err, posts) {
+//     if (err) {
+//         console.log(err)
+//     } else {
+//         console.log(posts)
+//     }
+// })
+//
+// post number not used for the dummy api
+http.delete('https://jsonplaceholder.typicode.com/posts/1', 1, function (err, posts) {
     if (err) {
         console.log(err)
     } else {
         console.log(posts)
     }
 })
-
 
--- easyhttp.js
+++ easyhttp.js
@@ -27,3 +27,31 @@ EasyHTTP.prototype.post = function (url, data, callback) {
     }
     this.http.send(JSON.stringify(data));
 }
+
+EasyHTTP.prototype.update = function (url, data, callback) {
+    this.http.open('PATCH', url, true)
+    this.http.setRequestHeader('Content-type', 'application/json')
+
+    let self = this
+    this.http.onload = function () {
+        callback(null, self.http.responseText)
+
+    }
+    this.http.send(JSON.stringify(data));
+}
+EasyHTTP.prototype.delete = function (url, id, callback) {
+    this.http.open('DELETE', url, true)
+    // this.http.setRequestHeader('Content-type', 'application/json')
+
+    let self = this
+    this.http.onload = function () {
+        if (self.http.status === 200) {
+            callback(null, 'Post deleted')
+        } else {
+            callback('Error: ' + self.http.status)
+        }
+    }
+
+
+    this.http.send(JSON.stringify(data));
+}
